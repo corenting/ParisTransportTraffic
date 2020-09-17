fc3a352fa59f6fedd7083767b470d56bc6bd6b8e
corenting <corenting@gmail.com>
2020-09-17 22:33:31 +0200

    Theme update

--- app/src/main/java/fr/corenting/traficparis/MainActivity.kt
+++ app/src/main/java/fr/corenting/traficparis/MainActivity.kt
@@ -1,7 +1,6 @@
 package fr.corenting.traficparis
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import fr.corenting.traficparis.ui.main.MainFragment
 
@@ -16,9 +15,5 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return false
     }
 }
