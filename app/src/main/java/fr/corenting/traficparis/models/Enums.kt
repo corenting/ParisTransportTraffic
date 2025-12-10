ba08d5a84c83126d54cda6a88e1f9558b87f933f
Corentin Garcia <corenting@gmail.com>
2025-12-10 17:49:33 +0100

    feat: 5.6

--- app/src/main/java/fr/corenting/traficparis/models/Enums.kt
+++ app/src/main/java/fr/corenting/traficparis/models/Enums.kt
@@ -6,7 +6,8 @@ enum class LineType(val apiName: String, val menuFilterId: Int) {
     METRO("metro", R.id.filter_metro),
     RER("rer", R.id.filter_rer),
     TRANSILIEN("transilien", R.id.filter_transilien),
-    TRAMWAY("tramway", R.id.filter_tramway)
+    TRAMWAY("tramway", R.id.filter_tramway),
+    OTHER("other", R.id.filter_other)
 }
 
 enum class LineState {
